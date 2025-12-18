2705c8630885dc77ac16d5d775e375d18bcaa6da
kishorkumar-b <kishorkumar.b@agaramtech.com>
2025-12-18 14:04:25 +0530

    changed in hello file

--- src/store/example/hello.jsx
+++ src/store/example/hello.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function hello() {
   return (
     <div><h1>Hello World</h1>
-        <p>This is the hello component.</p>
+        <p>This is the hello component.</p>\
+        
     </div>
   )
 }
