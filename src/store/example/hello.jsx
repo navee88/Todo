a4d631c9a4ab531192930b6d9ef4c41d552fe648
naveen.k <naveen.k@agaramtech.com>
2025-12-18 14:28:40 +0530

    update content in hello

--- src/store/example/hello.jsx
+++ src/store/example/hello.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 
 export default function hello() {
   return (
-    <div><h1>Hello World</h1>
-        <p>This is the hello component.</p>\
-
-
-        
+    <div><h1>Hello World</h1> 
     </div>
   )
 }
