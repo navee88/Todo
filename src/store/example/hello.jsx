7c6e6110257ece5424692a2367a5dd50f8d24324
kishorkumar-b <kishorkumar.b@agaramtech.com>
2025-12-18 14:14:31 +0530

    created hello file

--- src/store/example/hello.jsx
+++ src/store/example/hello.jsx
@@ -3,10 +3,7 @@ import React from 'react'
 export default function hello() {
   return (
     <div><h1>Hello World</h1>
-        <p>This is the hello component.</p>\
-
-
-        
+        <p>This is the hello component.</p>
     </div>
   )
 }
