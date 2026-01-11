f035a41bd72c6786ba8499440a5158050d5f3940
naveen.k <naveen.k@agaramtech.com>
2026-01-11 16:48:49 +0530

    file changes

--- src/store/exampl.jsx
+++ src/store/exampl.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Helloworld() {
   return (
+    <>
     <div>Helloworld from the world file</div>
+    <p>Hellow rofdsf</p>
+    <p>Hello World </p>
+    <p>Conferse fosdfa</p>
+    </>
   )
 }
 
